55dcc6bd71295ceff265178dbb8c6893d7dfe832
ArnavTicku <arnavticku01@gmail.com>
2025-09-02 01:19:03 +0530

    added paper routes

--- src/routes/paper.route.js
+++ src/routes/paper.route.js
@@ -1,12 +1,16 @@
-import {Router} from "express";
+import { Router } from "express";
+import { jwt_auth } from "../middlewares/auth.middleware.js";
+import { uploadPaperScholar, uploadPaperManual, getUserPapers, paperById, deletePaper, searchPaper } from "../controllers/paper.controller.js";
 import { upload_mul } from "../middlewares/multer.middleware.js";
-import {jwt_auth} from "../middlewares/auth.middleware.js";
-import { uploadPaperManual, uploadPaperScholar } from "../controllers/paper.controller.js";
 
-const paperRoute=Router()
-paperRoute.use(jwt_auth)
+const paperRoute = Router();
+paperRoute.use(jwt_auth);
 
-paperRoute.route("/addResearchPaper").post(uploadPaperScholar)
-paperRoute.route("/addResearchPaperManual").post(upload_mul.single("paper") , uploadPaperManual)
+paperRoute.route("/addResearchPaper").post(uploadPaperScholar);
+paperRoute.route("/addResearchPaperManual").post(upload_mul.single("paper"), uploadPaperManual);
+paperRoute.route("/getUserPapers").get(getUserPapers);
+paperRoute.route("/paperById/:id").get(paperById);
+paperRoute.route("/deletePaper/:id").delete(deletePaper);
+paperRoute.route("/searchPaper").get(searchPaper);
 
-export default paperRoute
+export default paperRoute;
